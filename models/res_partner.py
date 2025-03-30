6cf4b7c0f383ebdb18628b8f7a0655a6f26d6512
Michal Vondracek <info@michalvondracek.cz>
2025-03-30 05:54:33 +0200

    chore(model): res Partner has gps coords already

--- models/res_partner.py
+++ models/res_partner.py
@@ -5,5 +5,7 @@ class ResPartner(models.Model):
     _inherit = "res.partner"
 
     ruian_code = fields.Char("RUIAN Code")
-    ruian_coord_x = fields.Float("Coordinate X")
-    ruian_coord_y = fields.Float("Coordinate Y")
+    # ruian_coord_x = fields.Float("Coordinate X")
+    # ruian_coord_y = fields.Float("Coordinate Y")
+    # partner_latitude = fields.Float(string='Geo Latitude', digits=(10, 7))
+    # partner_longitude = fields.Float(string='Geo Longitude', digits=(10, 7))
